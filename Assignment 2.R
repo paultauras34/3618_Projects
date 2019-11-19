62d754f83cb546b3076f1be98580f0b642124327
paultauras34 <57158925+paultauras34@users.noreply.github.com>
2019-11-19 14:05:58 -0500

    Add files via upload

---
+++ Assignment 2.R
@@ -0,0 +1,32 @@
+#start with an annual effective interest rate of i=.03, n=10 for the term, and an innitial loan amount of L=200000.
+#Next, calculate the monthly effective interest rate to find out the monthly payments.
+#Use the vector (0,0,L) to represent our starting time. The goal is now to calculate the interest paid per period,the principal repaid per period, and the outstanding balance for each period.
+#We need to display all of this info in a table.
+
+
+n <- 10
+i <- 0.03
+L <- 200000
+i_monthly <- ((1+i)^(1/12))-1
+
+v <- 1/(1+i_monthly)
+a_angle <- ((1-(v^10))/i_monthly)
+R <- L/ (a_angle)
+
+x_vec <- c(0,0, L)
+X <- x_vec
+
+for(time in 1:n+1){
+I <- L * i_monthly
+P <- R - I
+L <- L - P
+x_vec <- c(round(I, digits = 2),round(P,digits = 2), round(L, digits = 2 ))
+X <- rbind(X,x_vec)
+}
+
+colnames = c("Interest Paid","Principal Paid", "Outstanding Balance")
+rownames = c("start","1","2","3","4","5","6","7","8","9","10")
+dimnames = list(rownames,colnames)
+X <- matrix(X,nrow = 11,ncol = 3,dimnames = dimnames)
+X
+
